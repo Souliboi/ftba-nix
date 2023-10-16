b3e7e1655ec53473cf907ed0cb426edf2cd5a19d
Souliboi <106060704+Souliboi@users.noreply.github.com>
2023-10-16 20:36:45 +0200

    add gitignore and a rough foundation

--- default.nix
+++ default.nix
@@ -38,4 +38,24 @@
 stdenv.mkDerivation rec {
   name = "ftb-app";
   version = "202310071959_918caed003";
+
+  let
+    # somehow replace $(version) with $(pkgver) 
+    # [replace from the ftba AUR package is ${pkgver//_/-}]
+    # and i have no idea how to do this
+  in
+
+  src = fetchurl {
+    url = "htpps://apps.modpacks.ch/FTBApp/release/$(version)/FTBA_linux_$(version).deb";
+    sha256 = "d795701bc1481e9e5797cd4e4026e01cb7e6f79990244483e366eb125e983180";
+  };
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+
+  meta = with lib; {
+    description = "FTB App for Linux to manage your Minecraft Modpacks";
+    homepage = "https://www.feed-the-beast.com/ftb-app";
+    platforms = with platforms; linux;
+    maintainers = with maintainers; [ souliboi ];
+  };
 }
